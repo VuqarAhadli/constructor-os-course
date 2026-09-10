4a3faab21b6ee8afe95b77af4f9a24555a6a278a
Vuq17 <vuqarahadli17@gmail.com>
2026-09-10 12:52:34 +0200

    change return types of cat functions to int

--- simplecat.c
+++ simplecat.c
@@ -10,10 +10,10 @@
 
 typedef struct 
 {
-	void (*catFunc)(void);
+	int (*catFunc)(void);
 }Cat;
 
-void getcCopy(void)
+int getcCopy(void)
 {
 	int ch;
 
@@ -22,73 +22,65 @@ void getcCopy(void)
 		if(putc(ch, stdout) == EOF)
 		{
 			perror("unexpected EOF");
-			return;
+			return 1;
 		}
 	}
 
 	if (ferror(stdin)) 
 	{
-		perror("could not open stdin"); 
+		perror("error reading from stdin"); 
+		return 1;
 	}
 
-	if(fflush(stdout) == EOF)
-	{ 
-		perror("improper ending of a sequence");
-	}
-
+	return 0;
 }
 
 
-void readCopy(void)
+int readCopy(void)
 {
 	char ch;
 	ssize_t size;
 
-	while((size = read(STDIN_FILENO, &ch, 1)) > 0)
+	while ((size = read(STDIN_FILENO, &ch, 1)) > 0)
 	{
-		ssize_t sent = 0;
-		
-		while(sent < size)
-		{
-			ssize_t current = write(STDOUT_FILENO, &ch + sent, size - sent);
-			
-			if(current == -1)
-			{
-				perror("unable to write\n");
-				return;
-			}
+		ssize_t current = write(STDOUT_FILENO, &ch, 1);
 
-			sent += current;
+		if (current != 1)
+		{
+			perror("unable to write");
+			return 1;
 		}
 	}
 
-	if(size == -1)
+	if (size == -1)
 	{
-		perror("couldnt read the file\n");
+		perror("couldn't read from stdin");
+		return 1;
 	}
+
+	return 0;
 }
 
-
-void sendFileCopy(void)
+int sendFileCopy(void)
 {
 	struct stat st;
 
 	if(fstat(STDIN_FILENO, &st) == -1)
 	{
 		perror("error reading from stdin\n");
-		return;
+		return 1;
 	}
 
 	off_t offset = 0;
 
 	while (offset < st.st_size)
 	{	
-		ssize_t sent = sendfile(STDOUT_FILENO, STDIN_FILENO, &offset, st.st_size);
+		ssize_t sent = sendfile(STDOUT_FILENO, STDIN_FILENO, &offset, st.st_size - offset);
 			
 		if (sent == -1)
 		{
 			perror("error sending from stdin to stdout\n");
-			return;
+			return 1;
 		}
 
 		if (sent == 0)
@@ -98,6 +90,8 @@ void sendFileCopy(void)
 
 
 	}
+
+	return 0;
 
 }
 
@@ -135,6 +129,6 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	cat.catFunc();
-	return 0;
+	int returnCode = cat.catFunc();
+	return returnCode;
 }
